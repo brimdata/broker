78281c0668e567cc43e867f875fc2b676d3b0083
Dominik Charousset <dominik.charousset@corelight.com>
2019-12-12 14:46:09 +0100

    Fix subscription management with filter_extend

--- include/broker/filter_type.hh
+++ include/broker/filter_type.hh
@@ -8,4 +8,15 @@ namespace broker {
 
 using filter_type = std::vector<topic>;
 
+/// Extends the filter `f` with `x` such that the filter contains the minimal
+/// set of subscriptions. For example, subscribing to `/foo/bar` does not change
+/// the filter when it already contains a subscription to `/foo`. Further,
+/// subscribing to `/foo` if the filter already contains `/foo/bar` replaces the
+/// existing entry with the less specific '/foo'.
+/// @return `true` if the filter changed, `false` otherwise.
+bool filter_extend(filter_type& f, const topic& x);
+
+/// Convenience function for calling `filter_extend` with each topic in `other`.
+bool filter_extend(filter_type& f, const filter_type& other);
+
 } // namespace broker
